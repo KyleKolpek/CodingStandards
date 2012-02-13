4bcd0eef05500fd7ee992a587c9b06287389dc48
Kyle Kolpek <kakolpek@gmail.com>
2012-02-13 15:46:10 -0500

    Converting comment styles

--- formatting.h
+++ formatting.h
@@ -11,65 +11,65 @@ namespace NewNamespace
 
 class OtherClass2; // Use forward declarations over includes where possible
 
-////////////////////////////////////////////////////////////////////////////////
-/// Templated class designed to show off code formatting.
-/// Things to note:
-///   -three consecutive slashes for doxygen compatibility
-///   -the mandatory newline after the block
-///   -the 80 character limit
-///   -the TODO doesn't appear since it is only preceded by 2 slashes
-///   -the brief description is the first line and must be delimited by a period
-///   -doxygen is used primarily (exclusively?) in the header file.
-//  TODO: Nothing
-////////////////////////////////////////////////////////////////////////////////
+/***************************************************************************//**
+ *  Templated class designed to show off code formatting.
+ *  Things to note:
+ *    -three consecutive slashes for doxygen compatibility
+ *    -the mandatory newline after the block
+ *    -the 80 character limit
+ *    -the TODO doesn't appear since it is only preceded by 2 slashes
+ *    -the brief description is the first line and must be delimited by a period
+ *    -doxygen is used primarily (exclusively?) in the header file.
+ ****************************************************************************//*
+ *  TODO: Nothing
+ ******************************************************************************/
 
 template <typename T>
 class NewClass: public OtherClass1  
 {
     public:
-        int publicIntVar;            ///< Description after member.
-        int* publicPointerVar;       ///< Description after member.
+        int publicIntVar;            /**< Description after member. */
+        int* publicPointerVar;       /**< Description after member. */
                 
-        ////////////////////////////////////////////////////////////////////////
-        /// Sample enum.
-        /// A longer description of the enum. Values are accessed as
-        /// NewClass::VAL0
-        ////////////////////////////////////////////////////////////////////////
-        
+        /*******************************************************************//**
+         * Sample enum.
+         * A longer description of the enum. Values are accessed as
+         * NewClass::VAL0
+         *******************************************************************/
         enum MemberEnum
         {
-            VAL0,                    ///< Description after value.
-            VAL1,                    ///< Description after value.
+            VAL0,                    /**< Description after value. */
+            VAL1,                    /**< Description after value. */
         };
         
-        ////////////////////////////////////////////////////////////////////////
-        /// Default constructor.
-        /// A longer description of the default constructor.
-        ////////////////////////////////////////////////////////////////////////
+        /*******************************************************************//**
+         * Default constructor.
+         * A longer description of the default constructor.
+         *******************************************************************/
         
         NewClass();
         
-        ////////////////////////////////////////////////////////////////////////
-        /// Constructor with inputs.
-        /// A longer description of the other constructor.
-        /// \param[in] publicIntVar
-        ///   The first parameter.
-        /// \param[in] publicPointerVar
-        ///    The second parameter.
-        ////////////////////////////////////////////////////////////////////////
+        /*******************************************************************//**
+         * Constructor with inputs.
+         * A longer description of the other constructor.
+         * \param[in] publicIntVar
+         *   The first parameter.
+         * \param[in] publicPointerVar
+         *    The second parameter.
+         *******************************************************************/
         
         NewClass(
             int publicIntVar,
             int* publicPointerVar);
         
-        ////////////////////////////////////////////////////////////////////////
-        /// Sample public member function.
-        /// A longer description of the void member function.
-        /// \param[in] parameter1
-        ///   The first parameter.
-        /// \param[in] parameter2
-        ///    The second parameter.
-        ////////////////////////////////////////////////////////////////////////
+        /*******************************************************************//**
+         * Sample public member function.
+         * A longer description of the void member function.
+         * \param[in] parameter1
+         *   The first parameter.
+         * \param[in] parameter2
+         *    The second parameter.
+         *******************************************************************/
         
         char publicMemberFunction(
             int parameter1,
@@ -78,27 +78,27 @@ class NewClass: public OtherClass1
     protected:
     
     private:
-        char* privateStringVar;      ///< Description after member
+        char* privateStringVar;      /**< Description after member. */
         
-        ////////////////////////////////////////////////////////////////////////
-        /// Sample private member function.
-        /// A longer description of the private member function. Note the const
-        /// correctness.
-        /// \param[in] parameter1
-        ///   The first parameter.
-        /// \return
-        ///   The return type.
-        ////////////////////////////////////////////////////////////////////////
+        /*******************************************************************//**
+         * Sample private member function.
+         * A longer description of the private member function. Note the const
+         * correctness.
+         * \param[in] parameter1
+         *   The first parameter.
+         * \return
+         *   The return type.
+         *******************************************************************/
         
         int privateMemberFunction(
             T const* const parameter1);
             
-        ////////////////////////////////////////////////////////////////////////
-        /// Sample pure virtual function.
-        /// A longer description of the pure virtual function.
-        /// \param[in] parameter1
-        ///   The first parameter.
-        ////////////////////////////////////////////////////////////////////////
+        /*******************************************************************//**
+         * Sample pure virtual function.
+         * A longer description of the pure virtual function.
+         * \param[in] parameter1
+         *   The first parameter.
+         *******************************************************************/
         
         virtual void pureVirtualFunction(
             int parameter1) = 0;
@@ -106,23 +106,23 @@ class NewClass: public OtherClass1
 }; // End NewClass
 
 
-////////////////////////////////////////////////////////////////////////////////
-/// Sample struct.
-/// A longer description of the struct.
-////////////////////////////////////////////////////////////////////////////////
+/***************************************************************************//**
+ * Sample struct.
+ * A longer description of the struct.
+ ****************************************************************************/
 
 struct NewStruct
 {
-    int value;         ///< Int value.
-    char* str;         ///< String.
+    int value;         /**< Integer value. */
+    char* str;         /**< String.    */
 }typedef NewStruct;
 
-////////////////////////////////////////////////////////////////////////////////
-/// nonMemberFunction brief description.
-/// A longer description of the non member function.
-/// \return
-///   Return value.
-////////////////////////////////////////////////////////////////////////////////
+/***************************************************************************//**
+ * nonMemberFunction brief description.
+ * A longer description of the non member function.
+ * \return
+ *   Return value.
+ ****************************************************************************/
         
 char const* nonMemberFunction();
 } // End NewNamespace
