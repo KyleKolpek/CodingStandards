83242a5c492ac7807b9723e0c9004795c67adc47
Kyle Kolpek <kakolpek@gmail.com>
2012-02-15 13:19:00 -0500

    Changed access specifier indentation

--- formatting.h
+++ formatting.h
@@ -27,82 +27,82 @@ class OtherClass2; // Use forward declarations over includes where possible
 template <typename T>
 class NewClass: public OtherClass1  
 {
-    public:
-        int publicIntVar;            ///< Description after member.
-        int* publicPointerVar;       ///< Description after member.
-                
-        /*******************************************************************//**
-         * Sample enum.
-         * A longer description of the enum. Values are accessed as
-         * NewClass::VAL0
-         *******************************************************************/
-        enum MemberEnum
-        {
-            VAL0,                    ///< Description after value. 
-            VAL1,                    ///< Description after value.
-        };
-        
-        /*******************************************************************//**
-         * Default constructor.
-         * A longer description of the default constructor.
-         *******************************************************************/
-        
-        NewClass();
-        
-        /*******************************************************************//**
-         * Constructor with inputs.
-         * A longer description of the other constructor.
-         * \param[in] publicIntVar
-         *   The first parameter.
-         * \param[in] publicPointerVar
-         *    The second parameter.
-         *******************************************************************/
-        
-        NewClass(
-            int publicIntVar,
-            int* publicPointerVar);
-        
-        /*******************************************************************//**
-         * Sample public member function.
-         * A longer description of the void member function.
-         * \param[in] parameter1
-         *   The first parameter.
-         * \param[in] parameter2
-         *    The second parameter.
-         *******************************************************************/
-        
-        char publicMemberFunction(
-            int parameter1,
-            char* parameter2);
-    
-    protected:
-    
-    private:
-        char* privateStringVar;      ///< Description after member.
-        
-        /*******************************************************************//**
-         * Sample private member function.
-         * A longer description of the private member function. Note the const
-         * correctness.
-         * \param[in] parameter1
-         *   The first parameter.
-         * \return
-         *   The return type.
-         *******************************************************************/
-        
-        int privateMemberFunction(
-            T const* const parameter1);
+public:
+    int publicIntVar;            ///< Description after member.
+    int* publicPointerVar;       ///< Description after member.
             
-        /*******************************************************************//**
-         * Sample pure virtual function.
-         * A longer description of the pure virtual function.
-         * \param[in] parameter1
-         *   The first parameter.
-         *******************************************************************/
+    /***********************************************************************//**
+     * Sample enum.
+     * A longer description of the enum. Values are accessed as
+     * NewClass::VAL0
+     ***********************************************************************/
+    enum MemberEnum
+    {
+        VAL0,                    ///< Description after value. 
+        VAL1,                    ///< Description after value.
+    };
+    
+    /***********************************************************************//**
+     * Default constructor.
+     * A longer description of the default constructor.
+     ***********************************************************************/
+    
+    NewClass();
+    
+    /***********************************************************************//**
+     * Constructor with inputs.
+     * A longer description of the other constructor.
+     * \param[in] publicIntVar
+     *   The first parameter.
+     * \param[in] publicPointerVar
+     *    The second parameter.
+     ***********************************************************************/
+    
+    NewClass(
+        int publicIntVar,
+        int* publicPointerVar);
+    
+    /***********************************************************************//**
+     * Sample public member function.
+     * A longer description of the void member function.
+     * \param[in] parameter1
+     *   The first parameter.
+     * \param[in] parameter2
+     *    The second parameter.
+     ***********************************************************************/
+    
+    char publicMemberFunction(
+        int parameter1,
+        char* parameter2);
+
+protected:
+
+private:
+    char* privateStringVar;      ///< Description after member.
+    
+    /***********************************************************************//**
+     * Sample private member function.
+     * A longer description of the private member function. Note the const
+     * correctness.
+     * \param[in] parameter1
+     *   The first parameter.
+     * \return
+     *   The return type.
+     ***********************************************************************/
+    
+    int privateMemberFunction(
+        T const* const parameter1);
         
-        virtual void pureVirtualFunction(
-            int parameter1) = 0;
-        
+    /***********************************************************************//**
+     * Sample pure virtual function.
+     * A longer description of the pure virtual function.
+     * \param[in] parameter1
+     *   The first parameter.
+     ***********************************************************************/
+    
+    virtual void pureVirtualFunction(
+        int parameter1) = 0;
+    
 }; // End NewClass
 
 
